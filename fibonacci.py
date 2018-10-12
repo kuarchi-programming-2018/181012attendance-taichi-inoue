81000c3b5537337611a738448fbf3f6286db3896
taichi-inoue <43875115+taichi-inoue@users.noreply.github.com>
2018-10-12 17:18:46 +0900

    出席課題
    フィボナッチ数列を書き直しました

--- fibonacci.py
+++ fibonacci.py
@@ -6,8 +6,8 @@ Created on Fri Oct 12 16:35:06 2018
 """
 
 #フィボナッチ数列の表示
-def fib(n):
-    a0=0
-    a1=1
-    a2=a0+a1
-    return n
+def fib(i):
+    x,y = 0,1
+    for i in range(0,i):
+        x,y = y,x+y
+    return x
